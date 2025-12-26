1eef14a37fa07633937220c6245a06939bcb8e2e
Rupam Roy <iamrupamroyy@gmail.com>
2025-12-26 22:32:22 +0530

    video player bug fixed: 26/12/2025

--- watch2gether/frontend/src/socket.js
+++ watch2gether/frontend/src/socket.js
@@ -6,6 +6,7 @@ import { io } from 'socket.io-client';
 // and the backend is on http://localhost:3001. CORS is needed.
 // In production, you would serve the frontend build from the Express server,
 // so they would share the same origin.
+// const URL = 'http://localhost:3001';
 const URL = 'https://watch2gether-backend-sjgr.onrender.com';
 
 export const socket = io(URL);
